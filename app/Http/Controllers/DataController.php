cb3dfa5cb5f348b74a46fb325118885d5821bfd4
horvath.mark1 <brryy0371@gmail.com>
2025-04-14 11:42:22 +0200

    v

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Lector;
+use App\Models\University;
 use Illuminate\Http\Request;
 
 class DataController extends Controller
 {
     public function getAllUniversities(){
-
+        $univiersities = University::orderBy('established_year', 'desc')->get();
+        return response()->json($univiersities);
     }
 
     public function getStat(){
